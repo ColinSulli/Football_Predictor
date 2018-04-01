2922df3687ce9d5baeaa43a335fed38a2182f693
goodboysworld <goodboysworld@gmail.com>
2018-04-01 15:49:16 -0400

    updated epl.py to read all seasons and react to single instance match

--- epl.py
+++ epl.py
@@ -4,42 +4,71 @@ path = os.getcwd()
 
 def main():
     seasons = ['13-14', '14-15', '16-17', '17-18']
-    season_file = open(path + '/processed/14-15_processed.csv')
-    csv_reader = csv.reader(season_file)
-
     matches = {}
-    row_num = 1
     magnitudes = [0.1, 0.1, 0.15, 0.20, 0.40]
 
-    for row in csv_reader:
-        p = {}
-        p['home'] = 0.0
-        p['draw'] = 0.0
-        p['away'] = 0.0
-        if row_num > 1:
-            home = row[1]
-            away = row[2]
-            home_goals = row[3]
-            away_goals = row[4]
-            result = row[5]
-            p['home'] = row[6]
-            p['draw'] = row[7]
-            p['away'] = row[8]
-            matches[home] = {}
-            matches[home][away] = {}
-            goal_diff = [abs(home_goals - away_goals)]
-            if goal_diff[0] > 4:
-                goal_diff[0] = 4
-            difference = (1 - p['home'])
-            coefficient = magnitudes[goal_diff[0]]
-            if p['home'] <= 0.2:
-                if result == 'H' or result == 'D':
-                    p['home'] += coefficient * difference
-                    p['draw'] = p['draw'] - (0.70 * coefficient * difference)
-                    p['away'] = p['away'] - (0.30 * coefficient * difference)
+    for season in seasons:
+        season_file = open(path + '/processed/' + season + '_processed.csv')
+        csv_reader = csv.reader(season_file)
+        row_num = 1
+        matches[season] = {}
+        for row in csv_reader:
+            if row_num > 1:
+                p = {}
+                p['home'] = 0.0
+                p['draw'] = 0.0
+                p['away'] = 0.0
+                home = row[1]
+                away = row[2]
+                home_goals = int(row[3])
+                away_goals = int(row[4])
+                result = row[5]
+                p['home'] = float(row[6])
+                p['draw'] = float(row[7])
+                p['away'] = float(row[8])
+                matches[season][home] = {}
+                goal_diff = [abs(home_goals - away_goals)]
+                if goal_diff[0] > 4:
+                    goal_diff[0] = 4
+                alter_probabilities(p, result, goal_diff, magnitudes)
+                matches[season][home][away] = [p['home'], p['draw'], p['away']]
+            row_num += 1
 
-        row_num += 1
 
-    print matches
+def alter_probabilities(p, result, goal_diff, magnitudes):
+    win_diff = 1 - p['home']
+    draw_diff = 1 - p['home']
+    away_diff = 1 - p['away']
+    coefficient = magnitudes[goal_diff[0]]
+    if p['home'] <= 0.2:
+        if result == 'H' or result == 'D':
+            home_increase = coefficient * win_diff
+            p['home'] += home_increase
+            draw_increase = (0.7 * coefficient * draw_diff)
+            p['draw'] += draw_increase
+            p['away'] = p['away'] - home_increase - draw_increase
+    elif p['home'] <= 0.4:
+        if result == 'H':
+            home_increase = coefficient * win_diff
+            p['home'] += home_increase
+            draw_increase = (0.7 * coefficient * draw_diff)
+            p['draw'] += draw_increase
+            p['away'] = p['away'] - home_increase - draw_increase
+        elif result == 'A':
+            away_increase = coefficient * away_diff
+            p['away'] += away_increase
+            p['home'] -= away_increase
+    else:
+        if result == 'H' and p['home'] <= 0.6:
+            home_increase = coefficient * win_diff
+            p['home'] += home_increase
+            p['away'] -= home_increase
+        elif result == 'D' or result == 'A':
+            draw_increase = coefficient * draw_diff
+            p['draw'] += draw_increase
+            away_increase = 0.7 * coefficient * draw_diff
+            p['away'] += away_increase
+            p['home'] = p['home'] - draw_increase - away_increase
+
 
 main()
