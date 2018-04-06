dde78bd306b556898fa7c65b6d34e17f63181d54
Abhi Muchhal <abhimuch@Abhimanyu-Muchhals-MacBook-Pro.local>
2018-04-06 18:30:04 -0400

    aggregate finished

--- aggregate.py
+++ aggregate.py
@@ -4,33 +4,47 @@ import sys
 #Get a triple dict
 HOME_TEAM = 1
 AWAY_TEAM = 2
+RESULT = 5
 HOME_WEIGHTING = .6
 AWAY_WEIGHTING = .4
-weighting [.1, .2, .3, .4]
+#weighting [.1, .2, .3, .4]
 seasons = ['13-14', '14-15', '16-17', '17-18']
 
 last_5 = {}
 
 def recent_form(home, away):
-
+	
 	with open(folder + filename, 'rb') as csvfile:
-    	csvreader = csv.reader(csvfile, delimiter=',')
+		csvreader = csv.reader(csvfile, delimiter=',')
 
     	for line in csvreader:
+    		# skip first line
+    		if "Date" in line[0]:
+    			continue
+
     		home = line[HOME_TEAM]
     		away = line[AWAY_TEAM]
+    		result = line[RESULT].strip()
 
     		if home not in last_5:
     			last_5[home] = []
     		if away not in last_5:
     			last_5[away] = []
 
-    		# append data
-    		last_5[home].append()
-    		last_5[away].append()
-
-
-			
+    		# home team wins
+    		print result
+    		if result == "H":
+    			last_5[home].append(1)
+    			last_5[away].append(-1)
+    		
+    		# away team wins
+    		elif result == "A":
+    			last_5[home].append(-1)
+    			last_5[away].append(1)
+    		# draw
+    		else:
+	    		last_5[home].append(0)
+	    		last_5[away].append(0)
 
 
 
